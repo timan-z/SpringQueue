8e86ea9753dccf1344721dbe76d2c5fb661987df
Timan Zheng <timanz100@gmail.com>
2025-10-24 12:49:59 -0400

    Initial commit (these are my starting files for my Spring Boot project -- initialized off of Spring Initializr)

---
+++ src/main/java/com/springqbackend/springqueue/SpringqueueApplication.java
@@ -0,0 +1,15 @@
+package com.springqbackend.springqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// NOTE-TO-SELF: In the context of remaking my GoQueue project, this wou
+
+@SpringBootApplication
+public class SpringqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringqueueApplication.class, args);
+	}
+
+}
