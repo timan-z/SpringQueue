a75fb318289a6d53572ebcc8a0b7ed53c7372206
Timan Zheng <timanz100@gmail.com>
2025-10-31 11:28:02 -0400

    Nevermind, seems the issue was just discrepancies between fields during serialization/deserialization. Rebooting the frontend fixed things (so there was probably some old cached code that was causing issues). Noted in types.ts

--- springqueue-frontend/src/utility/types.ts
+++ springqueue-frontend/src/utility/types.ts
@@ -1,4 +1,10 @@
-
+/* NOTE: Remember that for the data transfer (serialization/deserialization) into TypeScript, the fields
+must match exactly. I faced this problem here since the fields in my Task class in Java were camelCase while
+the fields here (types.ts) were PascalCase. Pretty sure TypeScript/JavaScript are supposed to actually be camelCase
+too, but Go is a little weird with its casing standards, so I made the fields below PascalCase. Obv, I've reverted
+that now and it seems to have fixed all of the issues I had with frontend-to-backend connectivity (only the data
+retrieval functions weren't working).
+*/
 export type Task = {
     id: string;
     payload: string;
